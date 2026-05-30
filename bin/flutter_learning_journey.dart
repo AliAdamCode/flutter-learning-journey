b04fc7eec233709d85ae60a6f7a038f119f754af
AliAdamCode <adam763763763@gmail.com>
2026-05-30 19:09:35 +0500

    Day 1: variables, print statements, and Dart basics

---
+++ bin/flutter_learning_journey.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_learning_journey/flutter_learning_journey.dart' as flutter_learning_journey;
+
+void main(List<String> arguments) {
+  print('Hello world of Flutter: ${flutter_learning_journey.calculate()}!');
+}
---
+++ test/flutter_learning_journey_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_learning_journey/flutter_learning_journey.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
