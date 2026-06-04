4518489241bec56256768d0a2784c97ed35dae6e
AliAdamCode <adam763763763@gmail.com>
2026-06-04 16:44:13 +0500

    Day 2: operators and basic logic exercises

--- exercises/day01_variables.dart
+++
@@ -1,8 +0,0 @@
-void main(){
-  print("Hello, Ali!");
-  print("I am learning Dart.");
-  print("Dart is a great programming language.");
-  print("I am 20 year old.");
-  print("I am a Student.");
-  
-}
