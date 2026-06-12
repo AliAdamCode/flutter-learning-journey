1be6d47b7feab3cfc99524cfee73d84c5c79852c
AliAdamCode <adam763763763@gmail.com>
2026-06-12 20:12:09 +0500

    day04_ loops and repetition exercises

---
+++ lib/day04_ loops and repetition exercises.dart
@@ -0,0 +1,33 @@
+void main() {
+  for(int i = 1 ; i <= 10 ; i++){
+    print (i);
+  }
+  for(int i = 10 ; i>= 1 ; i--)
+    {
+        print(i);
+    }
+for (int number = 1 ; number <= 50 ; number++){
+     if (number % 2 == 0){
+    print(number);
+}
+}
+int sum = 0;
+for (int i = 1 ; i<= 100 ; i ++){
+    sum +=i;
+}{
+    print('Sum: $sum');
+}
+for (int i = 1 ; i <= 10 ; i++){
+    print('7 * $i = ${7 * i}');
+}
+String star = '';
+for ( int i = 1 ; i<= 5 ; i++){
+    for (int j = 1 ; j <=i ; j++){
+ 
+ star += "*";
+  }
+print (star); 
+}
+
+    }
+
